be8f9674f151c4e9e8235530034b5795f4346b5b
Rishi Bagodi <rishibagodi@gmail.com>
2026-04-06 10:28:03 +0530

    Add emi.java

---
+++ emi.java
@@ -0,0 +1,7 @@
+System.out.println("================================");
+System.out.println("     EMI BURDEN ANALYZER");
+System.out.println("================================");
+System.out.println("Monthly Income : ₹" + income);
+System.out.println("Total EMI      : ₹" + totalEMI);
+System.out.println("EMI Ratio      : " + String.format("%.1f", ratio) + "%");
+System.out.println("--------------------------------");
